4989c698864e4ec697fa224ad7e875d0a4d16ebd
locust49 <lyazid.souad@gmail.com>
2022-02-21 23:26:57 +0100

    add ex01

---
+++ sources/Module-00/ex01/exec.py
@@ -0,0 +1,12 @@
+import sys
+
+if __name__ == '__main__':
+    args = sys.argv
+    len_args = len(args)
+    if len(args) > 1:
+        args.pop(0)
+        len_args = len(args)
+        for index, arg in enumerate(args[::-1]):
+            print(arg[::-1].swapcase(), end='')
+            if index < len_args - 1:
+                print(' ', end='')
