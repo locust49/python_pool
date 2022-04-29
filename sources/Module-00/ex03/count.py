7c354e4ec0db14658d0946b094c774995435cff5
locust49 <lyazid.souad@gmail.com>
2022-04-29 23:56:09 +0000

    add ex03

--- sources/Module-00/ex03/count.py
+++ sources/Module-00/ex03/count.py
@@ -1,11 +1,16 @@
+import string
+
 def text_analyzer(text=None, *args):
+    """This function counts the number of upper characters, lower characters, \
+punctuation and spaces in a given text."""
     try:
         assert len(args) == 0, 'ERROR'
     except AssertionError as error_message:
         print(error_message)
+        exit()
 
     if text is None:
-        text = str(input('What is the text to analyse?'))
+        text = str(input('What is the text to analyse?\n'))
 
     character_count = 0
     upper_count = 0
@@ -13,13 +18,21 @@ def text_analyzer(text=None, *args):
     punctuation_count = 0
     spaces_count = 0
 
-    if text == '':
-        answer = f'The text contains {character_count} characters:\
-                    - {upper_count} upper letters\
-                    - {lower_count} lower letters\
-                    - {punctuation_count} punctuation marks\
-                    - {spaces_count} spaces'
-        print(answer)
+    print(text)
+    character_count = len(text)
+    for character in text:
+        if character.isupper(): 
+            upper_count += 1
+        elif character.islower(): 
+            lower_count += 1
+        elif character in string.punctuation:
+            punctuation_count += 1
+        elif character.isspace(): 
+            spaces_count+= 1
 
-
-text_analyzer('h', 's')
+    answer = f'The text contains {character_count} characters:\
+                - {upper_count} upper letters\
+                - {lower_count} lower letters\
+                - {punctuation_count} punctuation marks\
+                - {spaces_count} spaces'
+    print(answer)
