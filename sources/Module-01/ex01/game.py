b93a73fd736f5c8509c42b19b797d056352c6056
sou3ada <slyazid@student.1337.ma>
2022-05-04 23:54:02 +0000

    add ex01

--- sources/Module-01/ex01/game.py
+++ sources/Module-01/ex01/game.py
@@ -1,8 +1,27 @@
 class GotCharacter:
     """Game of thrones characters class"""
-    def __init__(self, first_name=None, is_alive=None):
-        self.first_name = 'No name' if first_name is None else first_name
+    def __init__(self, first_name=None, is_alive=None, killed_enemies=None):
+        self.first_name = first_name
         self.is_alive = True if is_alive is None else is_alive
+        self.killed_enemies = 0 if killed_enemies is None else killed_enemies
+
+    def kill(self, enemy: "GotCharacter"):
+        if self == enemy:
+            print('Suicide is not allowed !')
+        elif isinstance(enemy, Undying):
+            print('Sorry, you can\'t kill the undying!')
+        elif enemy.is_alive is True:
+            print(f'{self.first_name} stabbed {enemy.first_name} to death.')
+            enemy.is_alive = False
+            self.killed_enemies += 1
+        else:
+            print('Enemy already dead, you\'re cheating!\nPenalty received.')
+            self.killed_enemies -= 1
+
+    def __str__(self):
+        return(f"I, called {self.first_name}, am \
+{'alive !' if self.is_alive is True else 'dead :(.'} \
+I already killed {self.killed_enemies} person.")
 
 
 class Lannister(GotCharacter):
@@ -17,6 +36,10 @@ class Lannister(GotCharacter):
 
     def die(self):
         self.is_alive = False
+
+    def __str__(self):
+        return super().__str__() + f'\nI\'m from the {self.family_name} \
+family. My moto is {self.house_words}.'
 
 
 class GracefordofHolyhall(GotCharacter):
@@ -35,13 +58,24 @@ class GracefordofHolyhall(GotCharacter):
     def die(self):
         self.is_alive = False
 
+    def __str__(self):
+        return super().__str__() + f'\nI\'m from the {self.family_name} \
+family. My moto is {self.house_words}.'
 
-class ThisIsAJoke(Lannister):
+
+class Undying(GotCharacter):
     """An undying class"""
     def __init__(self, first_name=None, is_alive=True):
         super().__init__(first_name=first_name, is_alive=is_alive)
-        self.family_name = 'This is a joke'
+        self.family_name = 'Undying'
         self.house_words = 'We shall not die.'
+
+    def print_house_words(self):
+        print(self.house_words)
 
     def die(self):
         print('Nothing to see here. We don\'t die !!')
+
+    def __str__(self):
+        return super().__str__() + f'\nI\'m from the {self.family_name} \
+family. My moto is {self.house_words}.'
