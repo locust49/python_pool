e4cc198c085e5fea4f28a8645034a0dad1831d58
locust49 <lyazid.souad@gmail.com>
2022-05-04 16:38:50 +0000

    add ex01

---
+++ sources/Module-01/ex01/game.py
@@ -0,0 +1,31 @@
+class GotCharacter:
+    """Game of thrones characters class"""
+    def __init__(self, first_name=None, is_alive=None):
+        self.first_name = 'No name' if first_name is None else first_name
+        self.is_alive = True if is_alive is None else is_alive
+
+
+class Lannister(GotCharacter):
+    """The Lannister's class.. unless they're not classy"""
+    def __init__(self, first_name=None, is_alive=True):
+        super().__init__(first_name=first_name, is_alive=is_alive)
+        self.family_name = 'Lannister'
+        self.house_words = 'Hear Me Roar!'
+
+    def print_house_words(self):
+        print(self.house_words)
+
+    def die(self):
+        self.is_alive = False
+
+
+class GracefordofHolyhall(Lannister):
+    """An undying class"""
+    def __init__(self, first_name=None, is_alive=True):
+        super().__init__(first_name=first_name, is_alive=is_alive)
+        self.family_name = 'Graceford of Holyhall'
+        self.house_words = 'Work Her Will'
+
+    def die(self):
+        self.is_alive = True
+        print('GracefordofHolyhall don\'t die !!')
